d6c82ff94ebf766f6c16d9203d5d62e8602d75ec
David Reyes <reyisacc@gmail.com>
2023-06-14 16:35:13 -0400

    Burguer menu added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("Funcionando");
+
+
+const menuIcon = document.getElementById('menu-icon');
+const menu = document.getElementById('menu');
+
+//Agrego un evento click al menú hamburguesa
+menuIcon.addEventListener('click', function () {
+    menu.classList.toggle('show-menu');
+});
+
+// Activar el iten seleccionado en el menú
+for (let i = 0; i < menu.children.length; i++) {
+    menu.children[i].addEventListener('click', function () {
+        menu.children[i].classList.add('active');
+        for (let a = 0; a < menu.children.length; a++) {
+            if (a !== i) {
+                menu.children[a].classList.remove('active');
+            }
+        }
+
+    })
+}
+
